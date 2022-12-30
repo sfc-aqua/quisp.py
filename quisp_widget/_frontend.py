ba952dd440a9737f36ca8a8b4a136ed2bd2ab43a
zigen <hrlclb@gmail.com>
2022-12-30 14:18:20 +0900

    initialize with cookiecutter

---
+++ quisp_widget/_frontend.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) zigen.
+# Distributed under the terms of the Modified BSD License.
+
+"""
+Information about the frontend package of the widgets.
+"""
+
+module_name = "jupyter-quisp-widget"
+module_version = "^0.1.0"
