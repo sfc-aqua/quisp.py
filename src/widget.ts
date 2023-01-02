a9ecc446d154d4362b5e0e4c1e89c56843f955ef
zigen <hrlclb@gmail.com>
2023-01-02 20:48:09 +0900

    inject ini and ned file

--- src/widget.ts
+++ src/widget.ts
@@ -125,6 +125,131 @@ export class ExampleModel extends DOMWidgetModel {
   async startQuisp() {
     const [wasmModule, emscriptenSource, packageData] = await this
       .loadingPromise;
+
+    const nedContent = `
+      package networks;
+
+      import modules.*;
+      import channels.*;
+      import ned.IdealChannel;
+      import ned.DatarateChannel;
+      import modules.Backend.Backend;
+      import modules.Logger.Logger;
+
+      network Custom {
+        submodules:
+        backend: Backend;
+        logger: Logger;
+        EndNodeA: QNode {
+            address = 1;
+            node_type = "EndNode";
+            @display("i=COMP;p=107,159");
+        }
+        EndNodeB: QNode {
+            address = 2;
+            node_type = "EndNode";
+            @display("i=COMP;p=107,234");
+        }
+        connections:
+        EndNodeA.port++ <--> ClassicalChannel {  distance = 20km; } <--> EndNodeB.port++;
+        EndNodeA.quantum_port++ <--> QuantumChannel {  distance = 20km; } <--> EndNodeB.quantum_port++;
+      }
+          `;
+    const iniContent = `
+
+[General]
+seed-set = \${runnumber}   # this is the default =0 results in the same seed every time
+sim-time-limit = 100s
+# Qnic
+#**.buffers = intuniform(7,7)
+image-path = "./quisp/images"
+**.logger.log_filename = "\${resultdir}/\${configname}-\${runnumber}.jsonl"
+
+**.h_gate_error_rate = 1/2000
+**.h_gate_x_error_ratio = 0
+**.h_gate_y_error_ratio = 0
+**.h_gate_z_error_ratio = 0
+
+**.Measurement_error_rate = 1/2000
+**.Measurement_x_error_ratio = 1
+**.Measurement_y_error_ratio = 1
+**.Measurement_z_error_ratio = 1
+
+**.x_gate_error_rate = 1/2000
+**.x_gate_x_error_ratio = 0
+**.x_gate_y_error_ratio = 0
+**.x_gate_z_error_ratio = 0
+
+**.z_gate_error_rate = 1/2000
+**.z_gate_x_error_ratio = 0
+**.z_gate_y_error_ratio = 0
+**.z_gate_z_error_ratio = 0
+
+
+#Error on Target, Error on Controlled
+**.cnot_gate_error_rate = 1/2000
+**.cnot_gate_iz_error_ratio = 1 #checked
+**.cnot_gate_zi_error_ratio = 1 #checked
+**.cnot_gate_zz_error_ratio = 1 #checked
+**.cnot_gate_ix_error_ratio = 1 #checked
+**.cnot_gate_xi_error_ratio = 1 #checked
+**.cnot_gate_xx_error_ratio = 1 #checked
+**.cnot_gate_iy_error_ratio = 1 #checked
+**.cnot_gate_yi_error_ratio = 1 #checked
+**.cnot_gate_yy_error_ratio = 1 #checked
+
+
+**.memory_x_error_rate = 1.11111111e-7
+**.memory_y_error_rate = 1.11111111e-7
+**.memory_z_error_rate = 1.11111111e-7
+**.memory_energy_excitation_rate = 0.000198
+**.memory_energy_relaxation_rate = 0.00000198
+**.memory_completely_mixed_rate = 0
+
+
+**.initial_notification_timing_buffer = 10 s #when to start the BSA timing notification.
+**.TrafficPattern = 1
+**.LoneInitiatorAddress = 1
+
+
+
+[Config Custom]
+network = networks.Custom
+seed-set = 0
+**.number_of_bellpair = 7000
+**.buffers = 100
+
+**.tomography_output_filename = "Example_run"
+
+
+**.emission_success_probability = 0.46*0.49
+
+# Error on optical qubit in a channel
+**.channel_loss_rate = 0.04500741397 # per km. 1 - 10^(-0.2/10)
+**.channel_x_error_rate = 0.01
+**.channel_z_error_rate = 0.01
+**.channel_y_error_rate = 0.01
+
+# Internal HOM in QNIC
+**.internal_hom_loss_rate = 0
+**.internal_hom_error_rate = 0 #Not inplemented
+**.internal_hom_required_precision = 1.5e-9 #Schuck et al., PRL 96,
+**.internal_hom_photon_detection_per_sec = 1000000000
+**.internal_hom_darkcount_probability = 10e-8 #10/s * 10^-9
+
+#Stand-alone HOM in the network
+**.hom_loss_rate = 0
+**.hom_error_rate = 0 #Not inplemented
+**.hom_required_precision = 1.5e-9 #Schuck et al., PRL 96,
+**.hom_photon_detection_per_sec = 1000000000
+**.hom_darkcount_probability = 10e-8 #1%
+
+**.link_tomography = false
+**.EndToEndConnection = true
+**.initial_purification = 2
+**.purification_type = 1001
+          `;
+
     // @ts-ignore
     globalThis.window.Module = {
       instantiateWasm: (imports, callback) => {
@@ -159,7 +284,14 @@ export class ExampleModel extends DOMWidgetModel {
         console.log('status changed: ', msg);
       },
       monitorRunDependencies: () => {},
-      preRun: [],
+      // @ts-ignore
+      preRun: [
+        () => {
+          console.log(FS.readdir('/networks'));
+          FS.writeFile('/networks/custom.ned', nedContent);
+          FS.writeFile('/networks/omnetpp.ini', iniContent);
+        },
+      ],
     };
     globalThis.window.qtenvReady = false;
     const timer = setInterval(() => {
@@ -180,7 +312,7 @@ export class ExampleModel extends DOMWidgetModel {
       '-f',
       './networks/omnetpp.ini',
       '-c',
-      'Example_run',
+      'Custom',
       '-r',
       '0',
       '--image-path=/quisp/images',
@@ -193,7 +325,8 @@ export class ExampleModel extends DOMWidgetModel {
         -1
       ) +
         'arguments_=' +
-        JSON.stringify(args)
+        JSON.stringify(args) +
+        ';'
     );
   }
 }
