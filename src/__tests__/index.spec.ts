d48e4ad97d3176fbf90c4858635b08fdc294fb42
zigen <hrlclb@gmail.com>
2023-01-10 21:35:47 +0900

    fix tests

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -6,20 +6,20 @@
 
 import { createTestModel } from './utils';
 
-import { ExampleModel } from '..';
+import { QuispIFrameModel } from '..';
 
 describe('Example', () => {
   describe('ExampleModel', () => {
     it('should be createable', () => {
-      const model = createTestModel(ExampleModel);
-      expect(model).toBeInstanceOf(ExampleModel);
+      const model = createTestModel(QuispIFrameModel);
+      expect(model).toBeInstanceOf(QuispIFrameModel);
       expect(model.get('value')).toEqual('Hello World');
     });
 
     it('should be createable with a value', () => {
       const state = { value: 'Foo Bar!' };
-      const model = createTestModel(ExampleModel, state);
-      expect(model).toBeInstanceOf(ExampleModel);
+      const model = createTestModel(QuispIFrameModel, state);
+      expect(model).toBeInstanceOf(QuispIFrameModel);
       expect(model.get('value')).toEqual('Foo Bar!');
     });
   });
